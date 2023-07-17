2fa6fa3557b7e064c0e39b6a145ed294ea768564
stalin velchael mendez <115166348+Velchael@users.noreply.github.com>
2023-07-17 11:47:15 -0300

    correccion de class error y error2

--- scripts.js
+++ scripts.js
@@ -7,15 +7,11 @@ function loadpk(){
         // Valor correcto, inicializar las variables y continuar con la lógica
         var errorNumeroPk = document.getElementById("errorNumeroPk");
         errorNumeroPk.innerHTML = ""; // Limpiar mensaje de error si existe
-        // Resto de la lógica...
     } else {
         // Valor incorrecto, mostrar mensaje de error
         var errorNumeroPk = document.getElementById("errorNumeroPk");
         errorNumeroPk.innerHTML = "Por favor, ingresa o número válido."; // Mensaje de error
     }
-
-
-	//var url="https://pokeapi.co/api/v2/pokemon-form/25";
 
 	var url="https://pokeapi.co/api/v2/pokemon/"+valorPk+"/";
 	
@@ -29,17 +25,13 @@ function loadpk(){
 			console.clear();
 			console.log(data);
 			document.getElementById('nome').innerHTML = data['name'];
-			//document.getElementById('numero').innerHTML = data['id'];
 			document.getElementById('abilidade1').innerHTML = data['abilities']['0']['ability']['name'];
             document.getElementById('abilidade2').innerHTML = data['abilities']['1']['ability']['name'];
-           			document.getElementById('experi').innerHTML = data['base_experience'];
+           	document.getElementById('experi').innerHTML = data['base_experience'];
 			
 			let img = data['sprites']['other']['dream_world']['front_default'];
 			document.getElementById('picFront').setAttribute('src',img);
 			
-			
-			// let imgBack = data['sprites']['back_default'];
-			// document.getElementById('picBack').setAttribute ('src',imgBack);
 		})
 
 	    .catch((erro) => {
@@ -47,13 +39,9 @@ function loadpk(){
 		});
 		
 }
-
-
-
 function loadpk2(){
 
 	var valorPk2= document.getElementById("numeroPk2").value;
-	//var url="https://pokeapi.co/api/v2/pokemon-form/25";
 
   // Validar el valor de numeroPk2
   if (valorPk2 !== "" && !isNaN(valorPk2) && valorPk2 > 0 && valorPk2 % 1 === 0) {
@@ -65,9 +53,6 @@ function loadpk2(){
 	var errorNumeroPk2 = document.getElementById("errorNumeroPk2");
 	errorNumeroPk2.innerHTML = "Por favor, ingresa o número válido."; // Mensaje de error
 }
-
-
-	
 	var url="https://pokeapi.co/api/v2/pokemon/"+valorPk2+"/";
 	
 	fetch (url)
@@ -80,18 +65,13 @@ function loadpk2(){
 			console.clear();
 			console.log(data);
 			document.getElementById('nome2').innerHTML = data['name'];
-			//document.getElementById('numero').innerHTML = data['id'];
 			document.getElementById('abilidade4').innerHTML = data['abilities']['0']['ability']['name'];
-            document.getElementById('abilidade5').innerHTML = data['abilities']['1']['ability']['name'];
-           
+            document.getElementById('abilidade5').innerHTML = data['abilities']['1']['ability']['name'];       
 			document.getElementById('experi2').innerHTML = data['base_experience'];
 			
 			let img = data['sprites']['other']['dream_world']['front_default'];
 			document.getElementById('picFront2').setAttribute('src',img);
 			
-			
-			// let imgBack = data['sprites']['back_default'];
-			// document.getElementById('picBack').setAttribute ('src',imgBack);
 		})
 
 	    .catch((erro) => {
